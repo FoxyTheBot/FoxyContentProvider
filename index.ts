fe0ef59be2faeb3084b61606def503b71e5555cf
leo <80543539+inex1stent@users.noreply.github.com>
2021-12-23 15:08:29 -0300

    k

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import express, { Application } from 'express';
 const app: Application = express();
+const port = 1337;
 
 app.use(express.static("public"));
 app.use("/", require("./routes/Other"));
 app.use("/", require("./routes/Memes"));
 app.use("/", require("./routes/Backgrounds"));
 
-app.listen(1337, () => {
+app.listen(port, () => {
     console.log("Server is running on port 1337");
 })
