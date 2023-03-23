7a4ed0df090c9c59cf29f455e53b79960a1562ac
WinG4merBR <wingamerbr@foxybot.win>
2023-03-23 19:28:59 -0300

    First commit 🎉

---
+++ index.ts
@@ -0,0 +1,7 @@
+import express, { Application } from 'express';
+
+const app: Application = express();
+
+app.use('/', require('./routes/index'));
+app.use('/memes', express.static('../assets/commands/memes'));
+app.listen(8080, () => console.info('Server started on port 8080'));
