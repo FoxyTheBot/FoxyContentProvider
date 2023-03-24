731a222c21d696800d54ad96800c628f8b2410bf
WinG4merBR <wingamerbr@foxybot.win>
2023-03-24 14:36:42 -0300

    Update directory

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ import express, { Application } from 'express';
 const app: Application = express();
 
 app.use('/', require('./routes/index'));
-app.use('/memes', express.static('./assets/commands/memes'));
-app.use('/image', express.static('./assets/commands/image'));
+app.use('/memes', express.static('./assets/commands/images/memes'));
+app.use('/image', express.static('./assets/commands/images'));
 app.listen(8080, () => console.info('Server started on port 8080'));
