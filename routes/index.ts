1a13ca1d198bea46728acb1c895dc873486d06d5
WinG4merBR <wingamerbr@foxybot.win>
2023-03-24 14:43:05 -0300

    Update directory²

--- routes/index.ts
+++ routes/index.ts
@@ -6,9 +6,9 @@ router.get("/image/:commandName", (req, res) => {
     const { commandName } = req.params;
 
     try {
-        const commandFiles = fs.readdirSync(`./assets/commands/image/${commandName}`);
+        const commandFiles = fs.readdirSync(`./assets/commands/images/${commandName}`);
         const asset = commandFiles[(Math.floor(Math.random() * commandFiles.length))]
-        res.send({ url: `https://cdn.foxybot.win/image/${commandName}/${asset}`});
+        res.send({ url: `https://cdn.foxybot.win/images/${commandName}/${asset}`});
     } catch(e) {
         res.status(404);
         console.error(e)
