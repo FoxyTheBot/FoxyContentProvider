c6234416df13b451b0fdcd230fb1a044b4dd4752
WinG4merBR <wingamerbr@foxybot.win>
2023-03-24 14:56:22 -0300

    Update `/images`

--- index.ts
+++ index.ts
@@ -4,5 +4,5 @@ const app: Application = express();
 
 app.use('/', require('./routes/index'));
 app.use('/memes', express.static('./assets/commands/images/memes'));
-app.use('/image', express.static('./assets/commands/images'));
+app.use('/images', express.static('./assets/commands/images'));
 app.listen(8080, () => console.info('Server started on port 8080'));
